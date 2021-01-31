b91e43a6cf6ed0fa1e7696129df4fae0b949f0ad
krassowski <krassowski.michal@gmail.com>
2021-01-31 21:22:41 +0000

    Turns out that properties are renamed only one way

--- jedi_language_server/server.py
+++ jedi_language_server/server.py
@@ -7,7 +7,7 @@ Official language server spec:
 """
 
 import itertools
-from typing import List, NamedTuple, Optional, Union
+from typing import List, Optional, Union
 
 from jedi import Project
 from jedi.api.refactoring import RefactoringError
@@ -120,12 +120,29 @@ SERVER = JediLanguageServer(protocol_cls=JediLanguageServerProtocol)
 
 @SERVER.feature(COMPLETION_ITEM_RESOLVE)
 def completion_item_resolve(
-    server: JediLanguageServer, item_params: NamedTuple
+    server: JediLanguageServer, params: CompletionItem
 ) -> CompletionItem:
     """Resolves documentation and detail of given completion item."""
     markup_kind = choose_markup(server)
-    params = item_params._asdict()
-    item = CompletionItem(**params)
+    # note: params is not a CompletionItem
+    # but a namedtuple complying with CompletionItem protocol
+    item = CompletionItem(
+        label=params.label,
+        kind=params.kind,
+        detail=params.detail,
+        documentation=params.documentation,
+        deprecated=params.deprecated,
+        preselect=params.preselect,
+        sort_text=params.sortText,
+        filter_text=params.filterText,
+        insert_text=params.insertText,
+        insert_text_format=params.insertTextFormat,
+        text_edit=params.textEdit,
+        additional_text_edits=params.additionalTextEdits,
+        commit_characters=params.commitCharacters,
+        command=params.command,
+        data=params.data,
+    )
     return jedi_utils.lsp_completion_item_resolve(
         item, markup_kind=markup_kind
     )
