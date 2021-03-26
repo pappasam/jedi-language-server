0b63ffd96535c55fd1be129a43e1baf686eee0ac
Sam Roeca <samuel.roeca@gmail.com>
2021-03-26 12:12:00 -0700

    Add cli options for logging and tcp control
    1. --tcp
    2. --host
    3. --port
    4. --log-file
    5. -v / --verbose

--- jedi_language_server/cli.py
+++ jedi_language_server/cli.py
@@ -1,7 +1,7 @@
 """Jedi Language Server command line interface."""
 
-
 import argparse
+import logging
 import sys
 
 from .server import SERVER
@@ -43,9 +43,57 @@ Examples:
     Run from stdio: jedi-language-server
 """,
     )
-    parser.add_argument("--version", action="store_true")
+    parser.add_argument(
+        "--version",
+        help="display version information and exit",
+        action="store_true",
+    )
+    parser.add_argument(
+        "--tcp",
+        help="use TCP server instead of stdio",
+        action="store_true",
+    )
+    parser.add_argument(
+        "--host",
+        help="host for TCP server (default 127.0.0.1)",
+        type=str,
+        default="127.0.0.1",
+    )
+    parser.add_argument(
+        "--port",
+        help="port for TCP server (default 2087)",
+        type=int,
+        default=2087,
+    )
+    parser.add_argument(
+        "--log-file",
+        help="redirect logs to the given file instead of writing to stderr",
+        type=str,
+    )
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        help="increase verbosity of log output",
+        action="count",
+        default=0,
+    )
     args = parser.parse_args()
     if args.version:
         print(get_version())
         sys.exit(0)
-    SERVER.start_io()
+    log_level = {0: logging.INFO, 1: logging.DEBUG}.get(
+        args.verbose,
+        logging.DEBUG,
+    )
+    if args.log_file:
+        logging.basicConfig(
+            filename=args.log_file,
+            filemode="w",
+            level=log_level,
+        )
+    else:
+        logging.basicConfig(stream=sys.stderr, level=log_level)
+    if args.tcp:
+        SERVER.start_tcp(host=args.host, port=args.port)
+    else:
+        SERVER.start_io()
