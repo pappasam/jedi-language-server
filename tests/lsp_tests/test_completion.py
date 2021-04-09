7c53f09538229a1cf3e7e274b818a78561cd49f9
Sam Roeca <samuel.roeca@gmail.com>
2021-04-09 13:11:06 -0700

    Add test addressing self in snippet completions

--- tests/lsp_tests/test_completion.py
+++ tests/lsp_tests/test_completion.py
@@ -113,3 +113,49 @@ def test_eager_lsp_completion() -> None:
             ],
         }
         assert_that(actual, is_(expected))
+
+
+def test_lsp_completion_class_method() -> None:
+    """Checks whether completion returns self unnecessarily.
+
+    References: https://github.com/pappasam/jedi-language-server/issues/121
+
+    Note: I resolve eagerly to make test simpler
+    """
+    with session.LspSession() as ls_session:
+        # Initialize, asking for eager resolution.
+        initialize_params = copy.deepcopy(VSCODE_DEFAULT_INITIALIZE)
+        initialize_params["initializationOptions"] = {
+            "completion": {"resolveEagerly": True}
+        }
+        ls_session.initialize(initialize_params)
+
+        uri = as_uri(COMPLETION_TEST_ROOT / "completion_test_class_self.py")
+        actual = ls_session.text_document_completion(
+            {
+                "textDocument": {"uri": uri},
+                "position": {"line": 7, "character": 13},
+                "context": {"triggerKind": 1},
+            }
+        )
+
+        # pylint: disable=line-too-long
+        expected = {
+            "isIncomplete": False,
+            "items": [
+                {
+                    "label": "some_method",
+                    "kind": 3,
+                    "detail": "def some_method",
+                    "documentation": {
+                        "kind": "markdown",
+                        "value": "```\nsome_method(x)\n\nGreat method.\n```\n",
+                    },
+                    "sortText": "z",
+                    "filterText": "some_method",
+                    "insertText": "some_method(${1:x})$0",
+                    "insertTextFormat": 2,
+                }
+            ],
+        }
+        assert_that(actual, is_(expected))
---
+++ tests/test_data/completion/completion_test_class_self.py
@@ -0,0 +1,8 @@
+class SomeClass:
+    def some_method(self, x):
+        """Great method."""
+        return x
+
+
+instance = SomeClass()
+instance.some
