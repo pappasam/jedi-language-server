3528b1f88ae144e5d05c4979c31277f909f3dbac
Anu Nadig <ckanupama@gmail.com>
2021-04-28 23:29:41 -0700

    Refactor notification handler

--- tests/lsp_test_client/session.py
+++ tests/lsp_test_client/session.py
@@ -60,6 +60,7 @@ class LspSession(MethodDispatcher):
         dispatcher = {
             PUBLISH_DIAGNOSTICS: self._publish_diagnostics,
             WINDOW_SHOW_MESSAGE: self._window_show_message,
+            WINDOW_LOG_MESSAGE: self._window_log_message,
         }
         self._endpoint = Endpoint(dispatcher, self._writer.write)
         self._thread_pool.submit(self._reader.listen, self._endpoint.consume)
@@ -223,23 +224,29 @@ class LspSession(MethodDispatcher):
 
     def _publish_diagnostics(self, publish_diagnostics_params):
         """Internal handler for text document publish diagnostics."""
-        fut = Future()
+        return self._handle_notification(
+            PUBLISH_DIAGNOSTICS, publish_diagnostics_params
+        )
 
-        def _handler():
-            callback = self.get_notification_callback(PUBLISH_DIAGNOSTICS)
-            callback(publish_diagnostics_params)
-            fut.set_result(None)
-
-        self._thread_pool.submit(_handler)
-        return fut
+    def _window_log_message(self, window_log_message_params):
+        """Internal handler for window log message."""
+        return self._handle_notification(
+            WINDOW_LOG_MESSAGE, window_log_message_params
+        )
 
     def _window_show_message(self, window_show_message_params):
-        """Internal handler for text document publish diagnostics."""
+        """Internal handler for window show message."""
+        return self._handle_notification(
+            WINDOW_SHOW_MESSAGE, window_show_message_params
+        )
+
+    def _handle_notification(self, notification_name, params):
+        """Internal handler for notifications."""
         fut = Future()
 
         def _handler():
-            callback = self.get_notification_callback(WINDOW_SHOW_MESSAGE)
-            callback(window_show_message_params)
+            callback = self.get_notification_callback(notification_name)
+            callback(params)
             fut.set_result(None)
 
         self._thread_pool.submit(_handler)
