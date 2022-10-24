f16cdd4bb084dff768f309a567694849a4721221
Felipe Lema <1232306+FelipeLema@users.noreply.github.com>
2022-10-24 16:32:49 -0300

    prefer python 3.8 code

--- jedi_language_server/constants.py
+++ jedi_language_server/constants.py
@@ -1,15 +1,15 @@
-"""Constants."""
-from typing import Annotated, List
+"""Constants.
 
-SEMANTIC_TOKENS: Annotated[
-    List[str],
-    """all tokens reported to client
+== SEMANTIC_TOKENS
+
+all tokens reported to client
 
 Not all tokens are to be used, so we may be missing some here
-
 https://microsoft.github.io/language-server-protocol/specifications/lsp/3.17/specification/#textDocument_semanticTokens
-""",
-] = [
+"""
+from typing import List
+
+SEMANTIC_TOKENS: List[str] = [
     "namespace",
     "type",
     "class",
