3ac54d89aaf05e1d5643bbaf991cdb7f8cdf97ab
TechPizza <23627133+TechPizzaDev@users.noreply.github.com>
2025-04-14 13:09:42 +0200

    Announce actual used tokens in SUPPORTED_SEMANTIC_TYPES

--- jedi_language_server/constants.py
+++ jedi_language_server/constants.py
@@ -5,15 +5,17 @@ from lsprotocol.types import SemanticTokenTypes
 MAX_CONCURRENT_DEBOUNCE_CALLS = 10
 """The maximum number of concurrent calls allowed by the debounce decorator."""
 
-_token_types = list(SemanticTokenTypes)
-
-SEMANTIC_TO_TOKEN_ID = {
-    "module": _token_types.index(SemanticTokenTypes.Namespace),
-    "class": _token_types.index(SemanticTokenTypes.Class),
-    "function": _token_types.index(SemanticTokenTypes.Function),
-    "param": _token_types.index(SemanticTokenTypes.Parameter),
-    "statement": _token_types.index(SemanticTokenTypes.Variable),
-    "property": _token_types.index(SemanticTokenTypes.Property),
+SEMANTIC_TO_TOKEN_TYPE = {
+    "module": SemanticTokenTypes.Namespace,
+    "class": SemanticTokenTypes.Class,
+    "function": SemanticTokenTypes.Function,
+    "param": SemanticTokenTypes.Parameter,
+    "statement": SemanticTokenTypes.Variable,
+    "property": SemanticTokenTypes.Property,
 }
 
-SUPPORTED_SEMANTIC_TYPES = list(map(lambda t: t.value, _token_types))
+SUPPORTED_SEMANTIC_TYPES = [t.value for t in SEMANTIC_TO_TOKEN_TYPE.values()]
+
+SEMANTIC_TO_TOKEN_ID = {
+    key: index for index, key in enumerate(SEMANTIC_TO_TOKEN_TYPE.keys())
+}
