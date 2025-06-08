6cf58686b83abbc6c37eabff5691d4b643d1cbc7
David Hotham <david.hotham@blueyonder.co.uk>
2025-06-08 16:19:46 +0100

    fix coverage report

--- noxfile.py
+++ noxfile.py
@@ -6,24 +6,26 @@ NOX_SESSION = nox.session(python=False)
 
 
 @NOX_SESSION
-def fix(session: nox.Session):
+def fix(session: nox.Session) -> None:
     """Fix files inplace."""
     session.run("ruff", "format", "-s", ".")
     session.run("ruff", "check", "-se", "--fix", ".")
 
 
 @NOX_SESSION
-def lint(session: nox.Session):
+def lint(session: nox.Session) -> None:
     """Check file formatting that only have to do with formatting."""
     session.run("ruff", "format", "--check", ".")
     session.run("ruff", "check", ".")
 
 
 @NOX_SESSION
-def typecheck(session: nox.Session):
+def typecheck(session: nox.Session) -> None:
     session.run("mypy", "jedi_language_server")
 
 
 @NOX_SESSION
-def tests(session: nox.Session):
-    session.run("slipcover", "-m", "pytest")
+def tests(session: nox.Session) -> None:
+    session.run(
+        "slipcover", "--source", "jedi_language_server", "-m", "pytest"
+    )
